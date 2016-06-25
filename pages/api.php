cd35db97d27ff1e621f83212a7eed507d524f5b6
philipp-r <philipp-r@users.noreply.github.com>
2016-06-25 13:32:23 +0200

    Use API as subpage of index.php

---
+++ pages/api.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// check authentication
+
+
+// check project GET
+
+// check POST
+
+/*
+
+if (isset($_POST['new_issue'])) {
+	$issues = Issues::getInstance("default");
+	$ans = $issues->new_issue($_POST, true);
+	var_dump($ans);
+}
+
+*/
+exit;
