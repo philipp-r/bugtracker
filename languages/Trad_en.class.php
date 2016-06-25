3cc679262d41ff6530b15e9fa8a2088d9867c1fc
philipp-r <philipp-r@users.noreply.github.com>
2016-06-25 16:51:49 +0200

    Bugfix

--- languages/Trad_en.class.php
+++ languages/Trad_en.class.php
@@ -222,7 +222,7 @@ This is an automated email, please do not reply.
 	const T_SEARCH = 'Search';
 	const T_API_SETTINGS = 'API';
 	const T_API_ACCESS_SETTINGS = 'API access';
-	const T_API_ACCESS_HELP = 'Create a new user and add him to group "bbapi". Give the group "bbapi" permission for every project you want to use with API and to create new issues. This user can only use the API, he cannot login.'
+	const T_API_ACCESS_HELP = 'Create a new user and add him to group "bbapi". Give the group "bbapi" permission for every project you want to use with API and to create new issues. This user can only use the API, he cannot login.';
 
 		# FORMS
 
--- languages/Trad_fr.class.php
+++ languages/Trad_fr.class.php
@@ -222,7 +222,7 @@ Ceci est un message automatisé. Merci de ne pas y répondre.
 	const T_SEARCH = 'Recherche';
 	const T_API_SETTINGS = 'API';
 	const T_API_ACCESS_SETTINGS = 'API access'; // je ne parle pas français
-	const T_API_ACCESS_HELP = 'Create a new user and add him to group "bbapi". Give the group "bbapi" permission for every project you want to use with API and to create new issues. This user can only use the API, he cannot login.' // je ne parle pas français
+	const T_API_ACCESS_HELP = 'Create a new user and add him to group "bbapi". Give the group "bbapi" permission for every project you want to use with API and to create new issues. This user can only use the API, he cannot login.'; // je ne parle pas français
 
 
 		# FORMS
