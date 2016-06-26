d56cb8a7227ffafc922bcd9d38f270681566cdb3
philipp-r <philipp-r@users.noreply.github.com>
2016-06-26 10:45:27 +0200

    API Key generators

---
+++ classes/api/keygen-travisci.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>keygen-travisci.php</title>
+	<meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+<h1>Travis CI API</h1>
+
+
+<?php
+if( !empty($_POST['submit']) ){
+
+$api_key = hash("sha256", $_POST['github_user']."/".$_POST['github_repo'].$_POST['travis_token']);
+?>
+<p>The key is: <em><?php echo $api_key; ?></em><br>
+Add this to the "api_config.php" file.</p>
+<hr>
+<?php
+}
+?>
+
+
+<form action="keygen-travisci.php" method="POST">
+	<p>GitHub username: <input type="text" name="github_user" id="github_user"></p>
+	<p>GitHub repository name: <input type="text" name="github_repo" id="github_repo"></p>
+	<p>Travis Token: <input type="text" name="travis_token" id="travis_token"></p>
+	<input type="submit" name="submit" id="submit" value="GENERATE">
+</form>
+
+</body>
+</html>
---
+++ classes/api/keygen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>keygen.php</title>
+	<meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+<?php
+// http://stackoverflow.com/a/4356295
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$api_key = generateRandomString(rand(18,28));
+?>
+
+<h1>Default API</h1>
+
+<p>Your API key is: <em><?php echo $api_key; ?></em><br>
+Use this as your "api_password" parameter.</p>
+
+<p>The md5 hash is: <em><?php echo md5($api_key); ?></em><br>
+Add this to the "api_config.php" file.</p>
+
+</body>
+</html>
