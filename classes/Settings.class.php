b3b10ac0ddef89ea5d95aa745e2e56ea92ce1510
philipp-r <philipp-r@users.noreply.github.com>
2016-06-26 11:24:40 +0200

    Fix

--- classes/Settings.class.php
+++ classes/Settings.class.php
@@ -506,7 +506,7 @@ class Settings {
 		//if (filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
 		// Validate email address not with filter, check only if there is an @-sign
 		// The email field is used to store GitHub webhook secret for API users
-		elseif (strpos($post['email'], '@') !== false) { // http://stackoverflow.com/a/4366748
+		if (strpos($post['email'], '@') !== false) { // http://stackoverflow.com/a/4366748
 			$email = $post['email'];
 		}
 		else {
