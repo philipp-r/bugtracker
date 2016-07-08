7bd19bd952b0fdd393438ff4d065c03e48f56c73
philipp-r <philipp-r@users.noreply.github.com>
2016-07-08 17:12:02 +0200

    Comment SendGird mail (issue #22)

--- classes/Mail.class.php
+++ classes/Mail.class.php
@@ -56,50 +56,50 @@ class Mail {
 		$object = '=?UTF-8?B?'.base64_encode($this->personal_object).'?=';
 
 
-		if(!empty($config['sendgrid_auth'])){
-			$data = Array ( 
-				"personalizations" => Array ( 
-					Array ( 
-						"to" => Array ( Array ( "email" => $to ) ),
-						"subject" => $object
-					) 
-				),
-				"from" => Array ( "email" => $from ),
-				"content" => Array ( 
-					Array ( 
-						"type" => "text",
-						"value" => $this->personal_content
-					) 
-				) 
-			);
-			$data_string = json_encode($data);
-		
-		
-			$ch = curl_init();
-			
-			curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Content-Type: application/json', 
-				'Authorization: Bearer '.$config['sendgrid_auth'],
-			    'Content-Length: '.strlen($data_string)
-			));
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-			curl_setopt($ch, CURLOPT_HEADER, true);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($ch);
-			$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			curl_close($ch);
-			// if not successful
-			if( $httpCode != "202" ){
-				// send normal mail
-				mail($to, $object, $this->personal_content, $headers);
-			}
-			return true;
-		}
-		else{
+		// if(!empty($config['sendgrid_auth'])){
+		// 	$data = Array ( 
+		// 		"personalizations" => Array ( 
+		// 			Array ( 
+		// 				"to" => Array ( Array ( "email" => $to ) ),
+		// 				"subject" => $object
+		// 			) 
+		// 		),
+		// 		"from" => Array ( "email" => $from ),
+		// 		"content" => Array ( 
+		// 			Array ( 
+		// 				"type" => "text",
+		// 				"value" => $this->personal_content
+		// 			) 
+		// 		) 
+		// 	);
+		// 	$data_string = json_encode($data);
+		// 
+		// 
+		// 	$ch = curl_init();
+		// 	
+		// 	curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
+		// 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		// 		'Content-Type: application/json', 
+		// 		'Authorization: Bearer '.$config['sendgrid_auth'],
+		// 	    'Content-Length: '.strlen($data_string)
+		// 	));
+		// 	curl_setopt($ch, CURLOPT_POST, true);
+		// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+		// 	curl_setopt($ch, CURLOPT_HEADER, true);
+		// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// 	$response = curl_exec($ch);
+		// 	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		// 	curl_close($ch);
+		// 	// if not successful
+		// 	if( $httpCode != "202" ){
+		// 		// send normal mail
+		// 		mail($to, $object, $this->personal_content, $headers);
+		// 	}
+		// 	return true;
+		// }
+		// else{
 			return mail($to, $object, $this->personal_content, $headers);
-		}
+		// }
 	}
 
 
