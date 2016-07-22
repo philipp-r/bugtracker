a21229cebfe225b9da8ecdad6bccdf7243b22b22
philipp-r <philipp-r@users.noreply.github.com>
2016-07-22 15:36:38 +0200

    Fix html lang

--- index.php
+++ index.php
@@ -435,10 +435,10 @@ if (!empty($config['link_privacypolicy'])) {
 
 <!DOCTYPE html>
 
-<!--[if lt IE 8]><html dir="ltr" lang="fr" class="ie lt8 lt9"><![endif]-->
-<!--[if IE 8]><html dir="ltr" lang="fr" class="ie ie8 lt9"><![endif]-->
-<!--[if IE 9]><html dir="ltr" lang="fr" class="ie ie9"><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html dir="ltr" lang="fr"><!--<![endif]-->
+<!--[if lt IE 8]><html dir="ltr" lang="<?php echo $config['language']; ?>" class="ie lt8 lt9"><![endif]-->
+<!--[if IE 8]><html dir="ltr" lang="<?php echo $config['language']; ?>" class="ie ie8 lt9"><![endif]-->
+<!--[if IE 9]><html dir="ltr" lang="<?php echo $config['language']; ?>" class="ie ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html dir="ltr" lang="<?php echo $config['language']; ?>"><!--<![endif]-->
 
 	<head>
 
@@ -471,7 +471,7 @@ if (!empty($config['link_privacypolicy'])) {
 
 	<body>
 
-		<?php echo $page->getAlerts(); ?>
+		<?php echo $page->getAlerts(); var_DUMP($config['language']); ?>
 		<!--[if lt IE 8]>
 			<div class="alert alert-error"><?php echo Trad::A_IE; ?></div>
 		<![endif]-->
