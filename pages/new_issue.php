89fd3f0b13db636aaa5c489aa52eca26eee8e970
philipp-r <philipp-r@users.noreply.github.com>
2016-08-05 14:22:45 +0200

    Issue template

--- pages/new_issue.php
+++ pages/new_issue.php
@@ -122,9 +122,52 @@ $content = '<h1>'.Trad::T_NEW_ISSUE.'</h1>'
 			.'<input type="text" name="issue_summary" value="'.$form_s.'" '
 				.'placeholder="'.Trad::F_SUMMARY.'" required />'
 			.'<textarea name="issue_text" rows="12" '
-				.'placeholder="'.Trad::F_CONTENT.'" required>'
-				.$form_t
-			.'</textarea>'
+
+if( !empty($form_t) ){
+	$content .=	$form_t;
+}
+elseif( getProject()=="bumpy-booby" ){
+$content .=	"[ Please describe your bug / feature request as detailed as possible. ]
+
+## Expected Behavior
+[ If you're describing a bug, tell us what should happen. 
+If you're suggesting a change/improvement, tell us how it should work. ]
+
+## Current Behavior
+[ If describing a bug, tell us what happens instead of the expected behavior.
+If suggesting a change/improvement, explain the difference from current behavior. ]
+
+## Possible Solution
+[ Not obligatory, but suggest a fix/reason for the bug, or ideas 
+how to implement the addition or change. Code examples are welcome. ]
+
+## Steps to Reproduce the bug
+[ Provide a link to a live example, or an unambiguous set of steps to reproduce this bug. 
+Include code to reproduce, if relevant. ]
+
+## My Environment
+ * Bumpy Booby version: [ version OR branch and commit ]
+ * Browser: [ your web browser ]
+ * Webserver: [ Apache / Lighttpd ]
+ * [ add more info if possible ]";
+}
+elseif( getProject()=="docs" ){
+$content .=	"[ Please describe the bug / enhancement as detailed as possible. ]";
+}
+elseif( getProject()=="support" ){
+$content .=	"[ Please describe your problem / question as detailed as possible. ]
+
+
+## Steps to Reproduce
+[ You can provide a link to a live example (your Bumpy Booby) here. ]
+
+## My Environment
+ * Bumpy Booby version: [ version OR branch and commit ]
+ * Browser: [ your web browser ]
+ * Webserver: [ Apache / Lighttpd ]
+ * [ add more info if possible ]";
+}
+$content .= '</textarea>'
 			.'<div class="preview text-container" style="display:none"></div>'
 			.$should_login;
 
